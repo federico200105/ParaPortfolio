9e98e4a3437c763964e92e7a88a0a95f5820aeb2
federico200105 <83227008+federico200105@users.noreply.github.com>
2021-07-22 16:23:30 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    //LOADER ANIMATION
+    document.getElementById("loading").style.opacity = "0";
+    document.getElementById("loading").style.visibility = "hidden";
+    document.body.style.overflowY = "auto";
+    window.scrollTo(0, 0);
+  };
